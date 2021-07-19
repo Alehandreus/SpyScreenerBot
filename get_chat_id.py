9b6fdc52e9f26cc13fb0fabde7a31310e5b32dc6
Alehandreus <49286540+Alehandreus@users.noreply.github.com>
2021-07-19 20:53:45 +0300

    Update get_chat_id.py

--- get_chat_id.py
+++ get_chat_id.py
@@ -4,12 +4,13 @@ import telebot
 token = input("Токен бота:\n")
 bot = telebot.AsyncTeleBot(token)
 print()
-print("Напишите боту в любом чате, он пришлет в ответ id чата")
+print("Напишите боту в любом чате")
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.chat.id)
+    print("Chat id:", message.chat.id)
 
 
 bot.polling()
